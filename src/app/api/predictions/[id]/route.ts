3ac5fb271c860912c7a4616264837b28978e9fc1
jchuharski <chuharsk@mit.edu>
2025-01-21 18:12:29 -0500

    lint

--- src/app/api/predictions/[id]/route.ts
+++ src/app/api/predictions/[id]/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-// import { type NextApiRequest } from 'next';
 import Replicate from "replicate";
 
 interface PredictionResponse {
@@ -13,19 +12,12 @@ const replicate = new Replicate({
   auth: process.env.API_KEY,
 });
 
-interface RouteParams {
-  params: {
-    id: string;
-  };
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
 export async function GET(
-//   request: NextRequest,
-  routeParams: RouteParams
+  request: Request,
+  { params }: { params: { id: string } }
 ) {
   try {
-    const prediction = await replicate.predictions.get(routeParams.params.id) as PredictionResponse;
+    const prediction = await replicate.predictions.get(params.id) as PredictionResponse;
 
     if (prediction?.error) {
       return NextResponse.json({ error: prediction.error }, { status: 500 });
